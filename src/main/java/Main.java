c8959629916419b2fc3f4750bc0abb40fe9d3543
Ruslan <m3mpm@users.noreply.github.com>
2025-10-07 19:19:47 +0700

    added time execution for the fourth solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,41 +2,42 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static void displayExecutionTime (long time) {
-        System.out.println("Время выполнения: " + time + " ns" + " ("
-                + TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS) + " ms)");
-    }
-
-
-
     public static void main(String[] args) {
-
-        long startTime1;
-        long startTime2;
 
         Solution solution = new Solution();
 
         String str1 = "swiss";
         String str2 = "😍 💕 I Ӝ love you Ӝ so much 😍";
 
-        startTime1 = System.nanoTime();
+        long startTime1 = System.nanoTime();
         char ch1 = solution.firstNonRepeatedCharacter1(str1);
-        startTime2 = System.nanoTime();
-        displayExecutionTime(startTime2 - startTime1);
+        long stopTime1= System.nanoTime();
+        displayExecutionTime(stopTime1 - startTime1);
         System.out.println(ch1);
 
-        startTime1 = System.nanoTime();
+        long startTime2 = System.nanoTime();
         char ch2 = solution.firstNonRepeatedCharacter2(str1);
-        startTime2 = System.nanoTime();
-        displayExecutionTime(startTime2 - startTime1);
+        long stopTime2 = System.nanoTime();
+        displayExecutionTime(stopTime2 - startTime2);
         System.out.println(ch2);
 
-        startTime1 = System.nanoTime();
+        long startTime3 = System.nanoTime();
         String ch3 = solution.firstNonRepeatedCharacter3(str2);
-        startTime2 = System.nanoTime();
-        displayExecutionTime(startTime2 - startTime1);
+        long stopTime3 = System.nanoTime();
+        displayExecutionTime(stopTime3 - startTime3);
         System.out.println(ch3);
 
+        long startTime4 = System.nanoTime();
+        String ch4 = solution.firstNonRepeatedCharacter4(str2);
+        long stopTime4 = System.nanoTime();
+        displayExecutionTime(stopTime4 - startTime4);
+        System.out.println(ch4);
+
+    }
+
+    private static void displayExecutionTime (long time) {
+        System.out.println("Время выполнения: " + time + " ns" + " ("
+                + TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS) + " ms)");
     }
 
 }
